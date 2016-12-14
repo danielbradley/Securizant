b591a8e2ed3e58b4272d4b12388c8fe0f165f9e5
Daniel Bradley <daniel@islabs.com>
2016-12-15 09:59:44 +1000

    Initial import.

---
+++ securizant/resources/software/libraries/core/min/2/readline.sh
@@ -0,0 +1,119 @@
+#!/tools/bin/bash
+#
+#       Copyright (c) 2004-2006 Daniel Robert Bradley. All rights reserved.
+#
+
+source /mnt/software/download.sh
+
+LIBRARIES_BASE=/system/software/libraries
+CATEGORY=terminal
+PACKAGE=readline
+VERSION=5.0
+ARCHIVE=tar.bz2
+UNZIP=-j
+
+URL=$RESOURCE_URL
+PKG_DIR=core/libraries
+PKG=$PACKAGE-$VERSION.$ARCHIVE  
+PATCH1=$PACKAGE-$VERSION-fixes-1.patch
+
+DEST=$LIBRARIES_BASE/$CATEGORY/$PACKAGE-$VERSION
+
+SOURCE=/mnt/source
+BUILD=/mnt/build/libraries
+
+#CHOST=i386-pc-linux-gnu
+
+main()
+{
+	echo Scripting $PACKAGE-$VERSION &&
+	prepare &&
+	unpack_package &&
+	patch_package &&
+	configure_package &&
+	make_package &&
+	install_package &&
+	complete
+}
+
+prepare()
+{
+	download ${URL} ${PKG_DIR} ${PKG}
+	download ${URL} ${PKG_DIR} ${PATCH1}
+        mkdir -p $LIBRARIES_BASE/$CATEGORY/$PACKAGE-$VERSION
+}
+
+unpack_package()
+{
+	if [ ! -d $BUILD/$PACKAGE-$VERSION ]
+	then
+		tar -C $BUILD -jxvf $SOURCE/$PKG_DIR/$PKG
+	fi
+}
+
+patch_package()
+{
+	if [ -f $BUILD/$PACKAGE-$VERSION/configure ] 
+	then
+		if [ ! -f $BUILD/$PACKAGE-$VERSION/SUCCESS.PATCHED ]
+		then
+			cd $BUILD/$PACKAGE-$VERSION &&
+			patch -Np1 -i $SOURCE/$PKG_DIR/$PATCH1 &&
+			touch $BUILD/$PACKAGE-$VERSION/SUCCESS.PATCHED
+		fi
+	fi
+}
+
+configure_package()
+{
+	if [ -f $BUILD/$PACKAGE-$VERSION/SUCCESS.PATCHED ]
+	then
+		if [ ! -f $BUILD/$PACKAGE-$VERSION/SUCCESS.CONFIGURE ]
+		then
+			cd $BUILD/$PACKAGE-$VERSION &&
+#			CFLAGS="-march=i386"
+			./configure \
+        	                --prefix=$LIBRARIES_BASE/$CATEGORY/$PACKAGE-$VERSION
+#				--enable-shared
+#				--host=$CHOST --target=$CHOST &&
+			touch $BUILD/$PACKAGE-$VERSION/SUCCESS.CONFIGURE
+		fi
+	fi
+}
+
+make_package()
+{
+	if [ -f $BUILD/$PACKAGE-$VERSION/SUCCESS.CONFIGURE ]
+	then
+		if [ ! -f $BUILD/$PACKAGE-$VERSION/SUCCESS.MAKE ]
+		then
+			cd $BUILD/$PACKAGE-$VERSION &&
+	                make SHLIB_XLDFLAGS=-lncurses &&
+			touch $BUILD/$PACKAGE-$VERSION/SUCCESS.MAKE
+		fi
+	fi
+}
+
+install_package()
+{
+	if [ -f $BUILD/$PACKAGE-$VERSION/SUCCESS.MAKE ]
+	then
+		if [ ! -f $BUILD/$PACKAGE-$VERSION/SUCCESS.INSTALL ]
+		then
+			cd $BUILD/$PACKAGE-$VERSION &&
+	                make install &&
+			chmod 755 $DEST/lib/lib{readline,history}.so* &&
+			touch $BUILD/$PACKAGE-$VERSION/SUCCESS.INSTALL
+		fi
+	fi
+}
+
+complete()
+{
+	if [ -f $BUILD/$PACKAGE-$VERSION/SUCCESS.INSTALL ]
+	then
+		rm -rf $BUILD/$PACKAGE-$VERSION/*
+	fi
+}
+
+main $@
