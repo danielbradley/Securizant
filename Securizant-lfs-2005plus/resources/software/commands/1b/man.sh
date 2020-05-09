bc89e94fd598dea5b46165917b344cfe4f81bdab
Daniel Bradley <daniel.bradley@islabs.com>
2020-05-09 16:26:55 +1000

    Updated man-db to 2.4.3.

--- Securizant-lfs-2005plus/resources/software/commands/1b/man.sh
+++ Securizant-lfs-2005plus/resources/software/commands/1b/man.sh
@@ -7,10 +7,10 @@ source /mnt/software/download.sh
 
 COMMAND_BASE=/system/software/commands
 CATEGORY=help
-PACKAGE=man
-VERSION=1.5p
-ARCHIVE=tar.bz2
-UNZIP=-j
+PACKAGE=man-db
+VERSION=2.4.3
+ARCHIVE=tar.gz
+UNZIP=-z
 
 URL=$RESOURCE_URL
 PKG_DIR=core/commands
@@ -55,11 +55,22 @@ patch_package()
 	then
 		if [ ! -f $BUILD/$PACKAGE-$VERSION/SUCCESS.PATCHED ]
 		then
-			cd $BUILD/$PACKAGE-$VERSION &&
-			sed -i 's@-is@&R@g' configure &&
-			sed -i 's@MANPATH./usr/man@#&@g' src/man.conf.in &&
-			sed -i 's@/usr@@' configure &&
-			sed -i 's#/usr/bin#@bindir@#' man2html/Makefile.in &&
+			cd $BUILD/$PACKAGE-$VERSION                        &&
+
+			mv man/de{_DE.88591,}                              &&
+			mv man/es{_ES.88591,}                              &&
+			mv man/it{_IT.88591,}                              &&
+			mv man/ja{_JP.eucJP,}                              &&
+
+			sed -i 's@\*_\*@??@'             man/Makefile.in   &&
+
+			sed -i '@\t\@usr\/man@d'         src/man_db.conf.in	&&
+
+			#sed -i 's@-is@&R@g'              configure         &&
+			#sed -i 's@/usr@@'                configure         &&
+			#sed -i 's@MANPATH./usr/man@#&@g' src/man.conf.in   &&
+			#sed -i 's#/usr/bin#@bindir@#' man2html/Makefile.in &&
+
 			touch $BUILD/$PACKAGE-$VERSION/SUCCESS.PATCHED
 		fi
 	fi
@@ -72,13 +83,14 @@ configure_package()
 		if [ ! -f $BUILD/$PACKAGE-$VERSION/SUCCESS.CONFIGURE ]
 		then
 			cd $BUILD/$PACKAGE-$VERSION &&
-#			CFLAGS="-march=i386"
-			./configure -confdir=/local/settings/software/commands/man \
+			./configure \
+				-confdir=/local/settings/software/commands/man \
 				-prefix=$DEST \
 				-bindir=$DEST/bin \
 				-mandir=$DEST/man \
-				-sbindir=$DEST/sbin &&
-#				--host=$CHOST --target=$CHOST &&
+				-sbindir=$DEST/sbin \
+				--enable-mb-groff \
+				--disable-setuid &&
 			touch $BUILD/$PACKAGE-$VERSION/SUCCESS.CONFIGURE
 		fi
 	fi
@@ -91,7 +103,7 @@ make_package()
 		if [ ! -f $BUILD/$PACKAGE-$VERSION/SUCCESS.MAKE ]
 		then
 			cd $BUILD/$PACKAGE-$VERSION &&
-	                make &&
+	        make &&
 			touch $BUILD/$PACKAGE-$VERSION/SUCCESS.MAKE
 		fi
 	fi
@@ -104,7 +116,7 @@ install_package()
 		if [ ! -f $BUILD/$PACKAGE-$VERSION/SUCCESS.INSTALL ]
 		then
 			cd $BUILD/$PACKAGE-$VERSION &&
-	                make install &&
+	        make install &&
 			touch $BUILD/$PACKAGE-$VERSION/SUCCESS.INSTALL
 		fi
 	fi
