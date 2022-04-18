1b771dad1b8f30ca44b6e8d722107d6d28289ee5
Daniel Bradley <daniel@crossadaptive.com>
2022-04-18 12:16:22 +1000

    Modified SetupBOS.sh - now checks out 'next' branch; and only builds if '--build' is passed.

--- szt/resources/static/default_user/SetupBOS.sh
+++ szt/resources/static/default_user/SetupBOS.sh
@@ -1,14 +1,42 @@
 #!/system/software/bin/bash
 
-cp GITConfig .gitconfig                                                       &&
-mkdir -p     /local/bos                                                       &&
-mount -L BOS /local/bos                                                       &&
-rm    -rf    /local/bos/*                                                     &&
-mkdir -p /local/bos/checkouts                                                 &&
-cd /local/bos/checkouts                                                       &&
-git clone https://github.com/danielbradley/BOS.git                            &&
-git clone https://github.com/danielbradley/Securizant.git                     &&
-cd /local/bos/checkouts/BOS/bos-6.1.1/bin                                     &&
-./setup_szt                                                                   &&
-./bos_all /local/bos/checkouts/Securizant/Securizant-lfs-2005/securizant.proj &&
-echo "Done"
+function main()
+{
+    cp GITConfig .gitconfig                                            &&
+    mkdir -p     /local/bos                                            &&
+    mount_local_bos                                                    &&
+    rm    -rf /local/bos/checkouts                                     &&
+    mkdir -p  /local/bos/checkouts                                     &&
+    cd /local/bos/checkouts                                            &&
+    git clone -b next https://github.com/danielbradley/BOS.git         &&
+    git clone -b next https://github.com/danielbradley/Securizant.git  &&
+    cd /local/bos/checkouts/BOS/bos                                    &&
+    do_build $@                                                        &&
+    echo "Done"
+}
+
+function mount_local_bos()
+{
+    local result=`grep "/local/bos" /system/processes/mounts`
+
+    if [ "" != "$result" ]
+    then
+        return 0;
+
+    else
+        mount -L BOS /local/bos
+        return $?
+    fi
+}
+
+function do_build()
+{
+    if [ "$1" == "--build" ]
+    then
+        cd /local/bos/checkouts/BOS/bos/bin &&
+        ./setup_szt                         &&
+        ./bos_all /local/bos/checkouts/Securizant/szt/securizant.proj
+    fi
+}
+
+main $@
