4afe6204043e0fcba553b82efc6b1813d83f1b3c
Daniel Bradley <daniel.bradley@islabs.com>
2022-11-05 18:09:50 +1000

    Update openssh.sh

--- szt/resources/software/services/openssh.sh
+++ szt/resources/software/services/openssh.sh
@@ -105,7 +105,7 @@ install_package()
 		then
 			cd $BUILD/$PACKAGE-$VERSION &&
 	                make install &&
-			sed -i 's|#PermitRootLogin prohibit-password|PermitRootLogin yes|' /local/settings/services/sshd &&
+			sed -i 's|#PermitRootLogin prohibit-password|PermitRootLogin yes|' /local/settings/services/sshd_config &&
 			touch $BUILD/$PACKAGE-$VERSION/SUCCESS.INSTALL
 		fi
 	fi
